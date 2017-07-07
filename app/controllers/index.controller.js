d7e4acddb1c29de26018c8b63036f8d42b1d4036
mosza16 <kittipong.kamnerd@mail.kmutt.ac.th>
2017-07-07 20:13:37 +0700

    Add files via upload

--- app/controllers/index.controller.js
+++ app/controllers/index.controller.js
@@ -1,16 +1,18 @@
 exports.render = function(req,res){
 	var isLoggedIn = false;
 	var email = '';
+	var youtube = '';
 	console.log('session : '+req.session.remember)
 	if(typeof req.session.remember !=='undefined'){
 		isLoggedIn = req.session.remember;
 		email = req.session.email;
-
+		youtube = req.session.youtube;
 	}
 	res.render('index',{
 		'title':'hi son',
 		'message':'how are things',
 		isLoggedIn : isLoggedIn,
 		email : email,
+		youtube : youtube
 	});
 }
--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -17,17 +17,18 @@ exports.login = function(req,res){
 		})
 		return;
 	}
-	if(req.body.remember ==='remember'){
-		req.session.remember = true;
-		req.session.email = req.body.email;
-		email = req.session.email;
-		req.session.on = 'im on';
-	}
 	if(youtube.indexOf("watch?v=")>-1){
 		youtube = youtube.replace("watch?v=","embed/")
 	}
 	if((autoplay*1)===1){
 		youtube = youtube.concat("?autoplay=1");
+	}
+	if(req.body.remember ==='remember'){
+		req.session.remember = true;
+		req.session.email = req.body.email;
+		email = req.session.email;
+		req.session.on = 'session is on';
+		req.session.youtube = youtube;
 	}
 	res.render('index',{
 		title:'Logged in as cookie1 is',
